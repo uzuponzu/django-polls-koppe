b501f7b9e722690ad5dfebe4a76cde0d43094553
uzponu <koppe@gmail.com>
2025-09-07 16:05:24 +0900

    藤原先生がむかつきそうなやつ

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    return HttpResponse("こっぺぱんこっぺぱんこっぺぱんこっぺぱんこっぺぱんこっぺぱんこっぺぱんこっぺぱん")
+def hobby(request):
+    return HttpResponse("私のhobbyは歩くことかもしれません")
+    
+def greet(reqest, name):
+    message =  "野球しようぜ", name,"お前空気な！"
+    return HttpResponse(message)
